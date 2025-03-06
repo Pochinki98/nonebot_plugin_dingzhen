2840faa8a1c1040664444ecb16390a41f0e2c122
Noobie III <109138085+Pochinki98@users.noreply.github.com>
2025-03-06 15:11:25 +0800

    Update __init__.py

--- nonebot_plugin_dingzhen/__init__.py
+++ nonebot_plugin_dingzhen/__init__.py
@@ -18,7 +18,7 @@ require("nonebot_plugin_localstore")
 __plugin_meta__ = PluginMetadata(
     name="丁真语音生成器",
     description="一款丁真语音生成器，用于合成丁真语音并发送",
-    usage="发送“丁真说 XX”即可命令机器人合成一段丁真语音并发出",
+    usage="发送“丁真/丁真说 XX”即可命令机器人合成一段丁真语音并发出",
     type="application",
     homepage="https://github.com/Pochinki98/nonebot_plugin_dingzhen",
     supported_adapters={"~onebot.v11"},
